c4ce6fb635ce966213b1bcc40c27b5a3197c0a10
ndmawpu <29minh11anh03@gmail.com>
2023-12-05 06:09:55 +0700

    change slashes

--- constant.py
+++ constant.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-df_FactSales = pd.read_csv('Data\FactSales.csv')
-df_FactListen = pd.read_csv('Data\FactListen.csv')
-df_FactGenre = pd.read_csv('Data\FactGenre.csv')
-df_DimAlbum = pd.read_csv('Data\DimAlbum.csv')
-df_DimDate = pd.read_csv('Data\DimDate.csv')
-df_DimEmployee = pd.read_csv('Data\DimEmployee.csv')
-df_DimCustomer = pd.read_csv('Data\DimCustomer.csv')
-df_DimTrack = pd.read_csv('Data\DimTrack.csv')
-df_DimGenre = pd.read_csv('Data\DimGenre.csv')
+df_FactSales = pd.read_csv(r'Data/FactSales.csv')
+df_FactListen = pd.read_csv(r'Data/FactListen.csv')
+df_FactGenre = pd.read_csv(r'Data/FactGenre.csv')
+df_DimAlbum = pd.read_csv(r'Data/DimAlbum.csv')
+df_DimDate = pd.read_csv(r'Data/DimDate.csv')
+df_DimEmployee = pd.read_csv(r'Data/DimEmployee.csv')
+df_DimCustomer = pd.read_csv(r'Data/DimCustomer.csv')
+df_DimTrack = pd.read_csv(r'Data/DimTrack.csv')
+df_DimGenre = pd.read_csv(r'Data/DimGenre.csv')
 
